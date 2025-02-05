a8989c88c40a15d51c2a2dfeb4b60cf98c2fe5dd
Achraf Noui <achraffnoui@gmail.com>
2025-02-05 23:54:39 +0100

    pulled branch achraf and added landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
 import Image from "next/image";
+import Head from 'next/head';
+import styles from './acceuil/Acceuil.module.css'
 
 export default function Home() {
   return (
-    
-      <div className="">homepage</div>
-    
+    <div className={styles.acceuilContainer}>
+      <Head>
+        <title>Accueil - JOBO</title>
+      </Head>
+      <div className={styles.acceuilContent}>
+        <h1 className={styles.acceuilTitle}>Bienvenue sur JOBO</h1>
+        <p className={styles.acceuilDescription}>
+          JOBO est une application innovante qui permet de prendre en photo des
+          objets industriels tels que des verres, couteaux, et bien plus, afin
+          d'obtenir des fiches métiers des artisans et ouvriers qui les ont
+          créés.
+        </p>
+        <p className={styles.acceuilDescription}>
+          Explorez le Dashboard pour découvrir les métiers et les artisans
+          derrière chaque objet.
+        </p>
+      </div>
+    </div>
   );
 }
